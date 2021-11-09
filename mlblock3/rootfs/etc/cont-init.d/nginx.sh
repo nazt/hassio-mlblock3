4cee00f2ffb8aa26c686ea562fbd07bbbab5c418
Nat W <nat.wrw@gmail.com>
2021-11-09 11:31:06 +0700

    1.4.0

--- mlblock3/rootfs/etc/cont-init.d/nginx.sh
+++ mlblock3/rootfs/etc/cont-init.d/nginx.sh
@@ -1,39 +1,29 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Home Assistant Community Add-on: grafana
-# Configures NGINX
+# Home Assistant Community Add-on: Node-RED
+# Configures NGINX for use with Node-RED
 # ==============================================================================
-declare port
-declare certfile
-declare grafana_user
-declare ingress_entry
-declare ingress_interface
-declare keyfile
+declare admin_port
 
-port=$(bashio::addon.port 80)
-if bashio::var.has_value "${port}"; then
+# Generate Ingress configuration
+bashio::var.json \
+    interface "$(bashio::addon.ip_address)" \
+    port "^$(bashio::addon.ingress_port)" |
+    tempio \
+        -template /etc/nginx/templates/ingress.gtpl \
+        -out /etc/nginx/servers/ingress.conf
+
+# Generate direct access configuration, if enabled.
+admin_port=$(bashio::addon.port 80)
+if bashio::var.has_value "${admin_port}"; then
     bashio::config.require.ssl
-
-    if bashio::config.true 'ssl'; then
-        certfile=$(bashio::config 'certfile')
-        keyfile=$(bashio::config 'keyfile')
-
-        mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
-        sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct.conf
-        sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct.conf
-
-    else
-        mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
-    fi
-    ingress_entry=$(bashio::addon.ingress_entry)
-    sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/direct.conf
+    bashio::var.json \
+        certfile "$(bashio::config 'certfile')" \
+        keyfile "$(bashio::config 'keyfile')" \
+        leave_front_door_open "^$(bashio::config 'leave_front_door_open')" \
+        port "^${admin_port}" \
+        ssl "^$(bashio::config 'ssl')" |
+        tempio \
+            -template /etc/nginx/templates/direct.gtpl \
+            -out /etc/nginx/servers/direct.conf
 fi
-
-ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-
-grafana_user='admin'
-if bashio::config.has_value 'grafana_ingress_user'; then
-    grafana_user=$(bashio::config 'grafana_ingress_user')
-fi
-sed -i "s/%%grafana_user%%/${grafana_user}/g" /etc/nginx/servers/ingress.conf
