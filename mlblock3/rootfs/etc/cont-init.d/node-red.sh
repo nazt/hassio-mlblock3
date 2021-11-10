7e41931ad912c5a6a0e293f4c42b35137cca4720
Nat W <nat.wrw@gmail.com>
2021-11-10 11:02:10 +0700

    Update 3.0.21

---
+++ mlblock3/rootfs/etc/cont-init.d/node-red.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Node-RED
+# Configures Node-RED before running
+# ==============================================================================
+declare port
+
+echo "this is hello, nodejs"
